cc63f1b7914f408c62b9635fb03dbc847366f114
Jesus Romero <jhromero@tekmexico.com>
2020-05-15 16:24:40 -0700

    Added access media test (WIP)

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+Object.defineProperty(window, 'navigator', {
+  writable: true,
+  value: jest.fn().mockImplementation(() => ({
+    getUserMedia: jest.fn(),
+  })),
+});
+
 test('Component renders without crashing.', () => {
   render(<App />);
+});
+
+test('Opens media without crashing.', async () => {
+  const app = render(<App />);
+  const openMediaButton = app.getByTestId('open-media');
+  openMediaButton.click();
 });
