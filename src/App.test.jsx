5a3d4d042db248a1e5bdb84cc4078c3dac50c2f9
Jesus Romero <jhromero@tekmexico.com>
2020-05-21 17:33:50 -0700

    Created tests that were sucessful and no errors.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,23 +1,46 @@
 import React from 'react';
-import { isElement } from 'react-dom/test-utils';
-import { act, render } from '@testing-library/react';
+import { act } from 'react-dom/test-utils'
+import { render, wait} from '@testing-library/react';
 import App from './App';
 
-Object.defineProperty(window.navigator, 'mediaDevices', {
-  value: {
-    getUserMedia: jest.fn()
-  }
-})
-Object.defineProperty(window, 'MediaStream', {
-  value: jest.fn()
-});
+describe("Main component testing", () => {
+  test('Basic component rendering.', () => {
+    const app = render(<App />);
+    const appComponent = app.getAllByTestId('main-app')
 
-test('Component renders without crashing.', () => {
-  render(<App />);
-});
+    expect(appComponent.length).toBe(1);
+  });
 
-test('Opens media without crashing.', () => {
-  const app = render(<App />);
-  const openMediaButton = app.getByTestId('open-media');
-  act(() => openMediaButton.click());
+  test('Should create state when rendering app', () => {
+    const mockDispatch = jest.fn();
+    React.useReducer = jest.fn((fn, state) => [state, mockDispatch]);
+
+    render(<App />);
+
+    expect(React.useReducer).toBeCalled();
+  });
+
+  test('Opens media without crashing.', async () => {
+    Object.defineProperty(window.navigator, 'mediaDevices', {
+      value: {
+        getUserMedia: jest.fn(()=>console.log('getUserMedia'))
+      }
+    });
+    Object.defineProperty(window, 'MediaStream', {
+      value: jest.fn(()=>console.log('mediastream'))
+    });
+    const mockDispatch = jest.fn(()=>console.log('dispatch'));
+    React.useReducer = jest.fn(()=>[{},mockDispatch]);
+
+    const app = render(<App />);
+    const openMediaButton = app.getByTestId('open-media');
+    await act( async ()=> {
+      openMediaButton.click();
+      await wait();
+    });
+
+    expect(window.navigator.mediaDevices.getUserMedia).toBeCalled();
+    expect(mockDispatch).toBeCalledTimes(2);
+    expect(window.MediaStream).toBeCalled();
+  });
 });
