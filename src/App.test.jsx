9348cb63ab3e572128a05b9172a7f9e0e58ba1cf
Jesus Romero <jhromero@tekmexico.com>
2020-05-21 17:35:54 -0700

    Minor syntax fixes. Removed logs.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { act } from 'react-dom/test-utils'
-import { render, wait} from '@testing-library/react';
+import { render, wait } from '@testing-library/react';
 import App from './App';
 
 describe("Main component testing", () => {
@@ -23,18 +23,18 @@ describe("Main component testing", () => {
   test('Opens media without crashing.', async () => {
     Object.defineProperty(window.navigator, 'mediaDevices', {
       value: {
-        getUserMedia: jest.fn(()=>console.log('getUserMedia'))
+        getUserMedia: jest.fn()
       }
     });
     Object.defineProperty(window, 'MediaStream', {
-      value: jest.fn(()=>console.log('mediastream'))
+      value: jest.fn()
     });
-    const mockDispatch = jest.fn(()=>console.log('dispatch'));
-    React.useReducer = jest.fn(()=>[{},mockDispatch]);
+    const mockDispatch = jest.fn();
+    React.useReducer = jest.fn(() => [{}, mockDispatch]);
 
     const app = render(<App />);
     const openMediaButton = app.getByTestId('open-media');
-    await act( async ()=> {
+    await act(async () => {
       openMediaButton.click();
       await wait();
     });
