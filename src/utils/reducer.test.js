78efbc8c8384a753b46898468425fe298e3815c8
Jesus Romero <jhromero@tekmexico.com>
2020-05-22 13:02:54 -0700

    Modified tests to reflect changes to reducer.

--- src/utils/reducer.test.js
+++ src/utils/reducer.test.js
@@ -3,25 +3,21 @@ import initialState from './initialState'
 import { SETINVITED } from './actions'
 
 describe('Reducer test', () => {
-    const state = reducer(initialState);
 
-    test('Should return initialState', () => {
-        expect(state).toMatchObject(initialState);
-    });
-
-    const modifiedState = reducer(state, { type: SETINVITED });
+    const modifiedState = reducer(initialState, { type: SETINVITED });
 
     test('Should update given state', () => {
-        expect(state.invited).toBe(false);
+        expect(initialState.invited).toBe(false);
         expect(modifiedState.invited).toBe(true);
     });
 
     test('Returned state should be different than given state', () => {
-        expect(state).not.toBe(modifiedState);
+        expect(initialState).not.toBe(modifiedState);
     });
 
-    test('Should return current state if action invalid or missing', () => {
-        const unmodifiedState = reducer(state, { type: '' })
-        expect(unmodifiedState).toBe(state);
+    test('Should throw error if action invalid or missing', () => {
+        expect(() => {
+            reducer(initialState, { type: '' })
+        }).toThrow();
     });
 });
