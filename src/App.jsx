787e5e97df189d531a8be7975a3ee2ccc4562c17
Jesus Romero <jhromero@tekmexico.com>
2020-06-01 17:44:26 -0700

    feat: added code that should allow conferencing

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,55 @@
 import React from 'react';
 import './App.css';
 
+import scaleVideos from './utils/scaleVideos';
+import config, { conferenceUI } from './utils/config'
+import captureUserMedia from './utils/captureUserMedia';
+
+const { location } = window;
+window.onresize = scaleVideos;
+
 function App() {
-    
+
+    React.useEffect(() => {
+        if (!location.hash.replace('#', '').length) {
+            location.href = location.href.split('#')[0] + '#' + (Math.random() * 100).toString().replace('.', '');
+            location.reload();
+        }
+    }, [])
+    const setupNewRoomButton = React.useRef();
+    const conferenceName = React.useRef();
+    const videosContainer = React.useRef();
+
     return (
         <article>
             <header >
-                <section class="experiment">
+                <section className="experiment">
                     <section>
-                        <span>
-                            <a href="/" target="_blank"
-                                title="Open this link in new tab. Then your conference room will be private!">
-                                <code>
-                                    <strong id="unique-token">#123456789
-                                </strong>
-                                </code>
-                            </a>
-                        </span>
+                        <h2 style={{
+                            textAlign: "center",
+                            display: "block"
+                        }}>
+                            <a href={location.href}>Right click to copy & share this private link</a>
+                        </h2>
 
-                        <input type="text" id="conference-name" placeholder="Conference Name" />
-                        <button id="setup-new-room" class="setup">Setup New Conference</button>
+                        <input type="text" ref={conferenceName} id="conference-name" placeholder="Conference Name" />
+                        <button id="setup-new-room" ref={setupNewRoomButton} className="setup" onClick={() => {
+                            setupNewRoomButton.current.disabled = true;
+                            conferenceName.current.disabled = true;
+                            captureUserMedia(config, videosContainer.current, () => {
+                                conferenceUI.createRoom({
+                                    roomName: (conferenceName || {}).value || 'Anonymous'
+                                });
+                            }, () => {
+                                setupNewRoomButton.current.disabled = false;
+                                conferenceName.current.disabled = false;
+                            })
+                        }}>Setup New Conference</button>
                     </section>
 
                     <table id="rooms-list"></table>
 
-                    <div id="videos-container"></div>
+                    <div id="videos-container" ref={videosContainer}></div>
                 </section>
             </header>
         </article>
