ee9e07de9f89e6b854e629c7fb89db3d63cb43cc
Jesus Romero <jhromero@tekmexico.com>
2020-06-16 17:04:10 -0700

    feat: added invited state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
 function App() {
+    // This checks if someone came from the main link
+    // or came through an invitation link
+    const [invited, setInvited] = React.useState(false);
+
     return (
         <article>
             <section id="new-conference">
-                <div id="conference-input">
+                <div id="conference-input" hidden={invited}>
                     <button id="setup-new-room" className="setup">Setup New Conference</button>
-                    <button id="invitation" className="invitation setup" disabled> Copy invitation</button>
+                    <button id="invitation" className="invitation setup" disabled>Copy invitation</button>
                 </div>
-                <button id="hang-up" className="setup" disabled> Hang Up</button>
+                <button id="hang-up" className="setup" disabled>Hang Up</button>
             </section>
 
             {/* list of all available conferencing rooms */}
